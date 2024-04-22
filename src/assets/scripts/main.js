a5f1e594c816d64bf1a7545e8555de25a5f0f95e
AlbertAndresVidueira <aandresvi@uoc.edu>
2024-04-22 10:58:57 +0200

    dependencia moment añadida

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,12 +6,16 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 import * as bootstrap from 'bootstrap';
-
+import moment from 'moment';
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
+/*+( function() {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+} )();*/
+
+const dateInput = document.getElementById('exampleInputdate');
+const fechaFormateada = moment().format('YYYY-MM-DD');
+dateInput.value = fechaFormateada;
